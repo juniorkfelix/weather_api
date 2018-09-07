6ad2bdf9ad87452e65f9cb0794f5b99713a6d1b3
juniorkfelix <felixonsare@gmail.com>
2018-09-07 22:03:41 +0300

    added footer and few other modifications

--- weather.js
+++ weather.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 					var widget = show(data);
 					$("#show").html(widget);
 					$("#city").val('');
-					console.log(data)
+					
 				}
 
 			});
@@ -29,6 +29,14 @@ $(document).ready(function(){
 	});
 });
 function show(data){
-	return  "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
-			 "<h3><strong>Weather</strong>: " + data.weather[0].description + "</h3>";
+	return 	 "<h3 style='font-size:40px; font-weight: bold;' class='text-center'>Current weather for :"+ data.name + ", " + data.sys.country + "</h3>" +
+			 "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+			 "<h3><strong>Description</strong>: " + data.weather[0].description + "</h3>" +
+			 "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
+			 "<h3><strong>Pressure</strong>: " + data.main.pressure + "</h3>" +
+			 "<h3><strong>Humidity</strong>: " + data.main.humidity + "</h3>" +
+			 "<h3><strong>Maximum Temperature</strong>: " + data.main.temp_max + "</h3>" +
+			 "<h3><strong>Minimum Temperature</strong>: " + data.main.temp_min + "</h3>" +
+			 "<h3><strong>Wind speed</strong>: " + data.wind.speed + "</h3>" +
+			 "<h3><strong>Wind direction</strong>: " + data.wind.deg + "</h3>";
 }
