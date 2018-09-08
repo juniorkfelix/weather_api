cfc4622ea172d0036b191ea6eaba67162f5cae91
juniorkfelix <felixonsare@gmail.com>
2018-09-08 10:18:33 +0300

    minor interface changes

--- weather.js
+++ weather.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 		}
 		else{
-			$("#error").html('field cannot be empty')
+			$("#error").html("<div class='alert alert-danger' id='errorCity'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Field cannot be empty</div>");
 		}
 
 	});
@@ -31,12 +31,12 @@ $(document).ready(function(){
 function show(data){
 	return 	 "<h3 style='font-size:40px; font-weight: bold;' class='text-center'>Current weather for :"+ data.name + ", " + data.sys.country + "</h3>" +
 			 "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
-			 "<h3><strong>Description</strong>: " + data.weather[0].description + "</h3>" +
-			 "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
-			 "<h3><strong>Pressure</strong>: " + data.main.pressure + "</h3>" +
-			 "<h3><strong>Humidity</strong>: " + data.main.humidity + "</h3>" +
-			 "<h3><strong>Maximum Temperature</strong>: " + data.main.temp_max + "</h3>" +
-			 "<h3><strong>Minimum Temperature</strong>: " + data.main.temp_min + "</h3>" +
-			 "<h3><strong>Wind speed</strong>: " + data.wind.speed + "</h3>" +
-			 "<h3><strong>Wind direction</strong>: " + data.wind.deg + "</h3>";
+			 "<h3><strong>Description</strong>: <img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'>" + data.weather[0].description + "</h3>" +
+			 "<h3><strong>Temperature</strong>: " + data.main.temp + "&deg;C</h3>" +
+			 "<h3><strong>Pressure</strong>: " + data.main.pressure + "hPa</h3>" +
+			 "<h3><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+			 "<h3><strong>Maximum Temperature</strong>: " + data.main.temp_max + "&deg;C</h3>" +
+			 "<h3><strong>Minimum Temperature</strong>: " + data.main.temp_min + "&deg;C</h3>" +
+			 "<h3><strong>Wind speed</strong>: " + data.wind.speed + "m/s</h3>" +
+			 "<h3><strong>Wind direction</strong>: " + data.wind.deg + "&deg;</h3>";
 }
